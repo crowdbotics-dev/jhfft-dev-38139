a35267a666f044998541828fea7545bda4c370a3
Crowdbotics <engineering@crowdbotics.com>
2022-10-11 15:23:23 +0000

    Initial commit.

---
+++ backend/jhfft_dev_38139/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jhfft_dev_38139 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jhfft_dev_38139.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "jhfft_dev_38139",
+  url: "https://jhfft_dev_38139.botics.co",
+  api: "https://jhfft_dev_38139.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
